b1fc06a22c87dcd20fb889427ea1a710acdb03f6
Théo RICHARD <mail@rcode.tech>
2023-01-31 20:00:10 +0100

    style: run prettier

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*", // Match any network id
+    networks: {
+        development: {
+            host: '127.0.0.1',
+            port: 8545,
+            network_id: '*', // Match any network id
+        },
     },
-  },
-  compilers: {
-    solc: {
-      version: "0.5.16",
+    compilers: {
+        solc: {
+            version: '0.5.16',
+        },
     },
-  },
 };
